a6b8c1c25d150ada6338a856b864f4716bada3e6
david <mikieta.dawid@gmail.com>
2023-04-30 13:57:46 +0200

    anders

--- resources/views/shipsink/show.blade.php
+++ resources/views/shipsink/show.blade.php
@@ -2,8 +2,8 @@
     <!-- um lokalen server zu starten sodass man die seite sieht schreibt "php artisan serve" in das terminal(ohne semicolons)-->
     @vite('resources/css/Table.css')
     @vite('resources/css/other.css')
-    <div class="ShipBox">
-        <div>
+    <div class="ShipBox" >
+        <div >
             <h2>Dein Feld</h2>
             <table id="table1">
             </table>
@@ -14,6 +14,7 @@
             </table>
         </div>
     </div> 
+    <button onclick="MarkMyShips()"></button>
     <script src="/JavaScript/variables_etc.js"></script>   
     <script src="/JavaScript/EnemieField.js"></script>
     <script src="/JavaScript/other.js"></script>
